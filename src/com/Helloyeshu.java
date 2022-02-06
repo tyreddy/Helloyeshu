de09fe7c56c5d9bae42054a4ba9501ad62aa1c6a
tysreddy <reddythumuyeshwanth.neo@gmail.com>
2022-02-06 19:46:29 +0530

    first java project

---
+++ src/com/Helloyeshu.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Helloyeshu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hai welcome to java programing");
+	}
+}
